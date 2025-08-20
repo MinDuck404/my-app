b3b2c84ad95d939d4545a7a3701399fc872a966e
MinDuck404 <duccjhin@gmail.com>
2025-08-20 09:44:44 +0700

    12_nuxt_seo

---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Welcome to My Movie Site</h1>
+        <p>Explore the latest movies and find your favorites!</p>
+        <nuxt-link to="/movies/test">Go to Test Movie Page</nuxt-link>
+    </div>
+</template>
---
+++ src/pages/movies/test.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useHead } from '#imports'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const movie = {
+  title: 'Avengers: Endgame',
+  year: 2019,
+  description: 'Xem Avengers Endgame full HD online'
+}
+
+useHead({
+  title: `${movie.title} (${movie.year}) | My Movie Site`,
+  meta: [
+    { name: 'description', content: movie.description },
+    { property: 'og:title', content: movie.title },
+    { property: 'og:description', content: movie.description },
+    { property: 'og:type', content: 'video.movie' },
+  ]
+})
+</script>
+
+<template>
+  <div>
+    <h1>{{ movie.title }} ({{ movie.year }})</h1>
+    <p>{{ movie.description }}</p>
+  </div>
+</template>
